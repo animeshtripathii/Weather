ac80a9c07d948ce65bf617346e116d4668de574c
Animesh Tripathi <tripathianimesh38@gmail.com>
2025-04-16 00:15:47 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,17 @@
+const apiKey="1fbe640edd72ac301972be5c0499d28a";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector("#input");
+const btn = document.querySelector("#submit");
+async function getWeatherData(city) {
+    const response=await fetch(apiUrl+city+`&appid=${apiKey}`);
+    const data=await response.json();
+
+    
+    document.querySelector(".city").innerText=data.name;
+    document.querySelector(".temp").innerText=Math.round(data.main.temp)+"°C";
+    document.querySelector(".humidity").innerText=data.main.humidity+"%";
+    document.querySelector(".wind").innerText=data.wind.speed+" km/h";
+}
+btn.addEventListener("click", () => {
+    getWeatherData(searchBox.value);
+});
